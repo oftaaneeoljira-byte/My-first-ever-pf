e27e97a2ac878323d0ff610e64e72e4371b81f4c
Oftaanee Oljira <oftaaneeoljira@gmail.com>
2026-04-05 23:35:57 +0300

    Modified CSS

---
+++ script.js
@@ -0,0 +1,5 @@
+
+// Smooth scroling
+document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
+    anchor.addEventListener('click', function (e) {e.preventDefault ();documennt.querySelector(this.getAttribute)})
+})
