bde185c1f7c910ca4abbf5177197b4353f0a9006
Oftaanee Oljira <oftaaneeoljira@gmail.com>
2026-04-23 14:09:34 +0300

    Updated

--- script.js
+++
@@ -1,5 +0,0 @@
-
-// Smooth scroling
-document.querySelectorAll('a[href^="#"]').forEach(anchor=>{
-    anchor.addEventListener('click', function (e) {e.preventDefault ();documennt.querySelector(this.getAttribute)})
-})
